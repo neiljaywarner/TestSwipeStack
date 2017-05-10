6d6c74371394968ca3ac22f597c87d8d05abaa75
Neil Jay Warner <neil.warner@fjordnet.com>
2017-05-10 18:44:28 -0500

    Add their default stuff that comes close to scrolling horizontally

--- sample/src/main/java/link/fls/swipestacksample/StackLayoutManager.java
+++ sample/src/main/java/link/fls/swipestacksample/StackLayoutManager.java
@@ -11,6 +11,7 @@ import android.view.View;
 public class StackLayoutManager extends RecyclerView.LayoutManager{
 
     private int verticalScrollOffset = 1;
+    private int horizontalScrollOffset = 0;
 
     @Override
     public RecyclerView.LayoutParams generateDefaultLayoutParams() {
@@ -75,4 +76,55 @@ public class StackLayoutManager extends RecyclerView.LayoutManager{
     private int getVerticalSpace() {
         return 33;
     }
+
+    @Override
+    public boolean canScrollHorizontally() {
+        return true;
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        return false;
+    }
+
+    @Override
+    public int scrollHorizontallyBy(int dx, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        int travel;
+        final int leftLimit = 0;
+        final int rightLimit = findRightLimit(); //a helper method to find the rightmost child's right side.
+        //-----> TODO: Modify this
+        if(dx + horizontalScrollOffset < leftLimit){
+            travel = horizontalScrollOffset;
+            horizontalScrollOffset = leftLimit;
+        }
+        else if(dx + horizontalScrollOffset + getHorizontalSpace() > rightLimit){
+            travel = rightLimit - horizontalScrollOffset - getHorizontalSpace();
+            horizontalScrollOffset = rightLimit - getHorizontalSpace();
+        }
+        else{
+            travel = dx;
+            horizontalScrollOffset += dx;
+        }
+        fillVisibleChildren(recycler);
+        return travel;
+    }
+
+    private int getHorizontalSpace() {
+        return this.getWidth() - this.getPaddingRight();
+    }
+
+    private int findRightLimit() {
+        // is it last visilbe or lst period?
+
+        // Either way, our scroll will be totally different
+        View thisView = this.getChildAt(getChildCount()-1);
+        return  thisView.getRight();
+    }
+
+    // had to fill out several functions that they didn't do for us
+
+    // STARTING WITH https://simpleandstupid.com/2015/05/01/recyclerview-and-its-custom-layoutmanager/
+
+    // Might be helpful - 3 part series
+    //http://wiresareobsolete.com/2014/09/building-a-recyclerview-layoutmanager-part-1/
 }
